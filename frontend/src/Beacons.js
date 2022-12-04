d1c63fa253a96f28a252eabbfdf0b9288bf15d8f
upsideon <65986739+upsideon@users.noreply.github.com>
2022-12-03 20:31:49 -0700

    Adding the ability to add beacons from the frontend.

--- frontend/src/Beacons.js
+++ frontend/src/Beacons.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
 
 import AddIcon from '@mui/icons-material/Add';
 import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
 import Checkbox from '@mui/material/Checkbox';
 import Fab from '@mui/material/Fab';
 import IconButton from '@mui/material/IconButton';
@@ -12,6 +19,7 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import TextField from '@mui/material/TextField';
 
 import {StatusCodes as HttpStatus} from 'http-status-codes';
 import { getHeaders } from './SignUp';
@@ -46,23 +54,88 @@ class Beacon extends React.Component {
   }
 }
 
+class AddBeaconForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {};
+    this.handleClose = this.props.handleClose;
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    const addressTextField = document.getElementById('address');
+    const address = addressTextField.value;
+    const response = await axios.post(
+      'http://localhost:8080/beacons',
+      { address },
+      getHeaders(),
+    );
+    this.handleClose();
+  }
+
+  render() {
+    return (
+       <Dialog
+         open={this.props.open}
+         onClose={this.handleClose}
+       >
+        <DialogTitle>Beacon Creation</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To create a beacon to notify others that you would like help shoveling,
+            please enter the following information.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="address"
+            label="Address"
+            type="address"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleClose}>Cancel</Button>
+          <Button onClick={this.handleSubmit}>Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
+
 class AddBeacon extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
     this.state = {
       beacons: [],
+      addModalOpen: false,
     };
+    this.addOnClick = this.addOnClick.bind(this);
     this.deleteOnClick = this.deleteOnClick.bind(this);
+    this.getBeacons = this.getBeacons.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
   async componentDidMount () {
+    const beacons = await this.getBeacons();
+    this.setState({ beacons });
+  }
+
+  async getBeacons() {
     const response = await axios.get(
       'http://localhost:8080/beacons',
       getHeaders(),
     );
     const beacons = JSON.parse(response.data);
-    this.setState({ beacons });
+    return beacons;
+  }
+
+  addOnClick() {
+    this.setState({ addModalOpen: true });
   }
 
   deleteOnClick(id) {
@@ -80,12 +153,24 @@ class AddBeacon extends React.Component {
     };
   }
 
+  async handleClose() {
+    this.setState({
+      addModalOpen: false,
+      beacons: await this.getBeacons(),
+    });
+  }
+
   render() {
     return (
       <div className="beacon-sections add-beacon">
         <h2>Add a Beacon</h2>
         <div className="add-beacon-subheader">
-          <Fab className="add-button" color="primary" aria-label="add">
+          <Fab
+            className="add-button"
+            onClick={this.addOnClick}
+            color="primary"
+            aria-label="add"
+          >
             <AddIcon />
           </Fab>
           <p className="add-description">If you are looking for help, add a beacon here.</p>
@@ -102,6 +187,7 @@ class AddBeacon extends React.Component {
             />
           })
         }
+        <AddBeaconForm open={this.state.addModalOpen} handleClose={this.handleClose}/>
       </div>
     );
   }
